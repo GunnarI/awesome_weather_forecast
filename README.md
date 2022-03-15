3f8ef0331ca316b8da881631aba6d9d7679859f8
Gunnar Ingi Fridriksson <gunnar.fridriksson@cubist.eu>
2022-03-15 17:54:40 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# awesome_weather_forecast
+
+Yet another weather app... but this one is the most awesome and it is open source!
+
---
+++ android/app/src/main/kotlin/dev/awesomeweathercorp/app/awesome_weather_forecast/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.awesomeweathercorp.app.awesome_weather_forecast
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
